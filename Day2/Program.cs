2d9e84aae714179d115561e88e0af50d34ce5ca8
dhaval pathak <dhaval_pathakk2001@yahoo.com>
2023-01-30 18:18:20 -0500

    added part A finished

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -5,19 +5,85 @@
         static void Main(string[] args)
         {
             System.IO.StreamReader streamReader = new StreamReader("data.txt");
-            string data;
-            
+            string? data;
+            List<FirstGroup> firstGroup = new List<FirstGroup>();
+            List<SecondGroup> secondGroup = new List<SecondGroup>();
+            int result = 0;
             try
             {
                 while ((data = streamReader.ReadLine()) != null)
                 {
+                    string[] inputs = data.Trim().Split(" ");
+                    Enum.TryParse(inputs[0].ToString(), out FirstGroup firsGroupVal);
+                    Enum.TryParse(inputs[1].ToString(), out SecondGroup secondGroupVal);
+                    firstGroup.Add(firsGroupVal);
+                    secondGroup.Add(secondGroupVal);
                 }
                 streamReader.Close();
+                for (int i = 0; i < firstGroup.Count; i++)
+                {
+                    result = result + DetermineResult(firstGroup[i], secondGroup[i]);
+                }
+
+                Console.WriteLine(result);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        public static int DetermineResult(FirstGroup first, SecondGroup second)
+        {
+            int result = 0;
+
+            switch (first)
+            {
+                case FirstGroup.A:
+                    switch (second)
+                    {
+                        case SecondGroup.X:
+                            result = Results.Draw + (int)second;
+                            break;
+                        case SecondGroup.Y:
+                            result = Results.Win + (int)second;
+                            break;
+                        case SecondGroup.Z:
+                            result = (int)second;
+                            break;
+                    }
+                    break;
+                case FirstGroup.B:
+                    switch (second)
+                    {
+                        case SecondGroup.X:
+                            result = (int)second;
+                            break;
+                        case SecondGroup.Y:
+                            result = Results.Draw + (int)second;
+                            break;
+                        case SecondGroup.Z:
+                            result = Results.Win + (int)second;
+                            break;
+                    }
+                    break;
+                case FirstGroup.C:
+                default:
+                    switch (second)
+                    {
+                        case SecondGroup.X:
+                            result = Results.Win + (int)second;
+                            break;
+                        case SecondGroup.Y:
+                            result = (int)second;
+                            break;
+                        case SecondGroup.Z:
+                            result = Results.Draw + (int)second;
+                            break;
+                    }
+                    break;
+            }
+            return result;
         }
     }
 }
---
+++ Day2/StrategyEnm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal enum StrategyEnm
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3
+    }
+
+    internal enum FirstGroup
+    {
+        A = StrategyEnm.Rock,
+        B = StrategyEnm.Paper,
+        C = StrategyEnm.Scissors
+    }
+
+    internal enum SecondGroup
+    {
+        X = StrategyEnm.Rock,
+        Y = StrategyEnm.Paper,
+        Z = StrategyEnm.Scissors
+    }
+
+    public static class Results
+    {
+        public static readonly int Win = 6;
+        public static readonly int Draw = 3;
+    }
+
+}

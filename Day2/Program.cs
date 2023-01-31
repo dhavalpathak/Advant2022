a54ba91d51ea2117cf3780235222e4f03499f438
dhaval pathak <dhaval_pathakk2001@yahoo.com>
2023-01-30 22:04:44 -0500

    second day finished

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -9,6 +9,7 @@
             List<FirstGroup> firstGroup = new List<FirstGroup>();
             List<SecondGroup> secondGroup = new List<SecondGroup>();
             int result = 0;
+            int resultSecond = 0;
             try
             {
                 while ((data = streamReader.ReadLine()) != null)
@@ -22,10 +23,12 @@
                 streamReader.Close();
                 for (int i = 0; i < firstGroup.Count; i++)
                 {
-                    result = result + DetermineResult(firstGroup[i], secondGroup[i]);
+                    result += DetermineResult1(firstGroup[i], secondGroup[i]);
+                    resultSecond += DetermineResult(firstGroup[i], secondGroup[i]);
                 }
 
                 Console.WriteLine(result);
+                Console.WriteLine(resultSecond);
             }
             catch (Exception ex)
             {
@@ -34,6 +37,63 @@
         }
 
         public static int DetermineResult(FirstGroup first, SecondGroup second)
+        {
+            int result = 0;
+
+            /*
+             * Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
+             */
+            switch (second)
+            {
+                case SecondGroup.X:
+                    
+                    switch (first)
+                    {
+                        case FirstGroup.A:
+                            result = (int)(SecondGroup.Z);
+                            break;
+                        case FirstGroup.B:
+                            result = (int)(SecondGroup.X);
+                            break;
+                        case FirstGroup.C:
+                            result = (int)(SecondGroup.Y);
+                            break;
+                    }
+                    break;
+                case SecondGroup.Y:
+                    switch (first)
+                    {
+                        case FirstGroup.A:
+                            result = 3 + (int)(SecondGroup.X);
+                            break;
+                        case FirstGroup.B:
+                            result = 3 + (int)(SecondGroup.Y);
+                            break;
+                        case FirstGroup.C:
+                            result = 3 + (int)(SecondGroup.Z);
+                            break;
+                    }
+                    break;
+                case SecondGroup.Z:
+                    switch (first)
+                    {
+                        case FirstGroup.A:
+                            result = 6 + (int)(SecondGroup.Y);
+                            break;
+                        case FirstGroup.B:
+                            result = 6 + (int)(SecondGroup.Z);
+                            break;
+                        case FirstGroup.C:
+                            result = 6 + (int)(SecondGroup.X);
+                            break;
+                    }
+                    break;
+            }
+            return result;
+        }
+
+
+        public static int DetermineResult1(FirstGroup first, SecondGroup second)
         {
             int result = 0;
 

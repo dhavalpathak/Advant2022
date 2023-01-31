9d8406b92c3e971b82abebbb690b90ca86d502e2
dhaval pathak <dhaval_pathakk2001@yahoo.com>
2023-01-30 22:53:24 -0500

    day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader streamReader = new StreamReader("data.txt");
+            string? data;
+
+            try
+            {
+                while ((data = streamReader.ReadLine()) != null)
+                {
+                   
+                }
+                streamReader.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

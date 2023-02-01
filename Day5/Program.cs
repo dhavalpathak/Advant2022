1f0a36e679beb1c02a20b802455c067e22805784
dhaval pathak <dhaval_pathakk2001@yahoo.com>
2023-02-01 12:44:26 -0500

    starting day 5

---
+++ Day5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader streamReader = new StreamReader("data.txt");
+            string? data;
+
+            try
+            {
+                while ((data = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(data);                   
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

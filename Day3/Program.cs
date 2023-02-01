cff904ae1cbb83717bf6adb6fd98c9bd40187a1a
dhaval pathak <dhaval_pathakk2001@yahoo.com>
2023-01-31 20:15:35 -0500

    second challange done

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -8,8 +8,10 @@
             string? data;
             List<string> fullData = new List<string>();
             List<Rucksack> rucksacks = new List<Rucksack>();
+            List<char> charsCommon = new List<char>();
             List<char> chars = new List<char>();
             int result = 0;
+            int resultSecond = 0;
 
             try
             {
@@ -42,13 +44,42 @@
                     result += GetCharNumber(c);
                 }
 
-                Console.WriteLine(result);
-                Console.WriteLine(fullData);
+                for (int i = 0; i < fullData.Count; i += 3)
+                {
+                   foreach (char  c in FindCommonSecond(new List<string>() { fullData[ i ], fullData[ i + 1], fullData[ i + 2 ] }))
+                    {
+                        charsCommon.Add(c);
+                    }
+                }
+
+                foreach (Char c in charsCommon)
+                {
+                    resultSecond += GetCharNumber(c);
+                }
+
+                Console.WriteLine("Challenge 1 Result := " + result);
+                Console.WriteLine("Challenge 2 Result := " + resultSecond);
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        public static List<char> FindCommonSecond(List<string> lstStr)
+        {
+            List<char> chars = new List<char>();
+            
+            foreach (char s in lstStr[0]) 
+            {
+                if (lstStr[1].Contains(s))
+                    if (lstStr[2].Contains(s))
+                        if (!chars.Contains(s))
+                            chars.Add(s);
+            }
+
+            return chars;
         }
 
         public static List<char> FindCommon(Rucksack rucksack)
